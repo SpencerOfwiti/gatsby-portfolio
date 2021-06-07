75cb41df51f4a72fe044a7d3e4ec7295bfaa02cc
Spencer Ofwiti <maxspencer56@gmail.com>
2021-06-07 14:12:53 +0300

    Create gatsby portfolio.

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,10 +10,11 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `content`,
+        path: `${__dirname}/src/content`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -32,5 +33,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-styled-components`,
   ],
 }
---
+++ src/content/hero.md
@@ -0,0 +1,9 @@
+---
+greetings: "Hello"
+emoji: "👋"
+title: "I'm Spencer Ofwiti"
+subtitlePrefix: "I build and design "
+subtitleHighlight: "things for the web."
+---
+
+Software Engineer and Open-Source Contributor. Based in Nairobi, Kenya.
